ac44f8564ccd6cec7f6b47f1ab434dbd40c9a91a
saedpanah <m.saedpanah76@gmail.com>
2022-12-13 19:14:16 +0330

    changes project

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
 import React  , {useState} from "react";
 import { Link } from "react-router-dom";
+import { Button } from "../Button/Button";
 
 const Navbar = ()=>{
   const [click , setClick]  = useState(false)
+  const [button , setButton] = useState(true)
   
   const handleClick = () =>{
     setClick(!click)
+  }
+
+  const closeMobileMenu = () =>{
+    setClick(false)
   }
     return(
         <>
@@ -17,6 +23,29 @@ const Navbar = ()=>{
                 <div className="menu-icon" onClick={handleClick}>
                   <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                  <li className="nav-item">
+                    <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                    Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/services" className="nav-link" onClick={closeMobileMenu}>
+                    Services
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/products" className="nav-link" onClick={closeMobileMenu}>
+                    Products
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/sign-up" className="nav-link-mobile" onClick={closeMobileMenu}>
+                    Sign Up
+                    </Link>
+                  </li>
+                </ul>
+                {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
             </div>
 
           </nav>
