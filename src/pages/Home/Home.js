e9afd56dcc63d2e77f03ba736d7c2dc261537615
saedpanah <m.saedpanah76@gmail.com>
2022-12-17 18:40:52 +0330

    project changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import HeroSection from "../../components/HeroSection/HeroSection";
+import Cards from "../../components/Cards/Cards";
 
 import '../../App.css'
+
 
 const Home = ()=>{
     return (
         <div>
             <HeroSection/>
+            <Cards/>
         </div>
     )
 }
