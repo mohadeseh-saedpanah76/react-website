0deb405a6f7ed11e531b6a1063489c3138c89ed2
saedpanah <m.saedpanah76@gmail.com>
2022-12-17 10:41:14 +0330

    project changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+
 import './App.css';
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route path='/'/>
+          <Route path='/' element={<Home/>}/>
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import HeroSection from "../../components/HeroSection/HeroSection";
+
+import '../../App.css'
+
+const Home = ()=>{
+    return (
+        <div>
+            <HeroSection/>
+        </div>
+    )
+}
+
+export default Home
